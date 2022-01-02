196b402f9cf828669d0528578ccab5ba906d2168
alireza olama <alireza.lm69@gmail.com>
2022-01-02 22:06:54 +0330

    second page optimal structure

--- lib/registeration.dart
+++ lib/registeration.dart
@@ -5,46 +5,73 @@ class RegisterProduct extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Text(
-            "Cadastro de Produtos",
-            style: TextStyle(fontSize: 28),
-          ),
-          TextField(
-            textAlign: TextAlign.left,
-            decoration: InputDecoration(
-              labelText: "Nome: ",
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.orange,
-                width: 2,
-              )),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Cadastro de Produtos",
+              style: TextStyle(fontSize: 24),
             ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Descrição: ",
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.orange,
-                width: 2,
-              )),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: const TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  labelText: "Nome: ",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  )),
+                ),
+              ),
             ),
-          ),
-          TextField(
-             decoration: InputDecoration(
-              labelText: "Valor: ",
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.orange,
-                width: 2,
-              )),
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: const TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  labelText: "Descrição: ",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  )),
+                ),
+              ),
             ),
-          )
-        ],
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: const TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  labelText: "Valor: ",
+                  hintText: "R\$ 0,00",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  )),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: Center(
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Enviar'),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 100),
+                      primary: Colors.orange,
+                    )),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
